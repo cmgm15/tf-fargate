01d2a3bda5865c545b7084091ba1bfc8d1940d8c
Carlos Mario Gómez Mejía <cmgomezm15@gmail.com>
2020-04-12 13:09:35 -0500

    Update remote_state module

--- base/state.tf
+++ base/state.tf
@@ -3,7 +3,7 @@
  */
 
 module "remote_state" {
-  source = "github.com/cmgm15/tf-s3-remote-state?ref=1.0.0"
+  source = "github.com/cmgm15/tf-s3-remote-state?ref=1.0.1"
 
   owner       = var.owner
   application = var.app
